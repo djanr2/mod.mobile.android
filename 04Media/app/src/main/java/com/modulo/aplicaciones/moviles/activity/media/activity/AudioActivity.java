6788b3d1cd553b0b032ec2a1cc033c992126b541
djanr2 <djanr2@hotmail.com>
2021-05-04 23:48:08 -0500

    04Media audio play

--- 04Media/app/src/main/java/com/modulo/aplicaciones/moviles/activity/media/activity/AudioActivity.java
+++ 04Media/app/src/main/java/com/modulo/aplicaciones/moviles/activity/media/activity/AudioActivity.java
@@ -1,7 +1,34 @@
 package com.modulo.aplicaciones.moviles.activity.media.activity;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+
+import com.modulo.aplicaciones.moviles.activity.media.R;
+
 
 public class AudioActivity extends Activity {
+    private MediaPlayer mp;
 
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+        mp = MediaPlayer.create(this, R.raw.yolo);
+    }
+
+    public void stopAudio(View view){
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
+    }
+
+    public void startAudio(View view){
+        mp.start();
+    }
 }
