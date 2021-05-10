5516ff4915731df4f6f00ace32e1313e9b2c6f93
djanr2 <djanr2@hotmail.com>
2021-05-10 00:55:13 -0500

    06 Hilos de ejecucion AsynkTaskPictures

---
+++ 06AsynkTaskPictures/app/src/main/java/com/modulo/aplicaciones/moviles/activity/asynktaskpictures/MainActivity.java
@@ -0,0 +1,14 @@
+package com.modulo.aplicaciones.moviles.activity.asynktaskpictures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
